0eeb02ab326a5f6a588c47c337e58f951e433190
ArehoneM <arehonecharlotte@gmail.com>
2025-05-23 14:42:49 +0200

    added auth, edit, ...

--- blogg/urls.py
+++ blogg/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
+
+# Allow logout via GET
+class LogoutViewGET(LogoutView):
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
+    # Auth
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutViewGET.as_view(next_page='post_list'), name='logout'),
 ]
--- myblog/settings.py
+++ myblog/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / 'static'
 
+
+LOGIN_REDIRECT_URL = 'post_list'
+LOGOUT_REDIRECT_URL = 'post_list'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
